53a568b9ea6c6907f53e21eddddb87c78e38c4f3
Paul Vallis <vallis1@gmail.com>
2018-09-15 18:42:32 +0100

    Add create-react-app client

---
+++ web/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+/* eslint-disable react/jsx-filename-extension */
+ReactDOM.render(<App />, document.getElementById('root'));
+/* eslint-enable react/jsx-filename-extension */
+
+registerServiceWorker();
