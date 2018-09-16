476eae36eeb5706eb80c6951c69795234713ed20
Paul Vallis <vallis1@gmail.com>
2018-09-16 19:17:36 +0100

    Accept requets from all origins (for now)

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,18 @@
 import dotenv from 'dotenv';
 import Koa from 'koa';
 import Router from 'koa-router';
+import cors from '@koa/cors';
 
 import queries from './data/queries';
 
 dotenv.load();
 
 const log = console.log; // eslint-disable-line
+
+// TODO cors config based on env vars
+const corsConfig = {
+  origin: '*',
+};
 
 const app = new Koa();
 const router = new Router();
@@ -15,6 +21,7 @@ router.get('/', (ctx => queries.boilerplate()
   .then(msg => { ctx.body = msg; })
   .catch(log)));
 
+app.use(cors(corsConfig));
 app.use(router.routes());
 
 // TODO use env variable for server port
