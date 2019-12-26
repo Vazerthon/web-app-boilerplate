ae9fbdc7010ea1022074aa3b0228c82ab3d07cad
Paul Vallis <vallis1@gmail.com>
2019-12-26 10:44:32 +0000

    Remove redux. Update deps

--- web/src/index.js
+++ web/src/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Switch, Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
-import store from './state';
-import history from './state/history';
+import history from './history';
 
-import Home from './containers/Home';
+import Home from './components/Home';
 
 /* eslint-disable react/jsx-filename-extension */
-ReactDOM.render((
-  <Provider store={store}>
-    <Router history={history}>
-      <Switch>
-        <Route path="/" exact render={() => <Home />} />
-      </Switch>
-    </Router>
-  </Provider>
-), document.getElementById('root'));
+ReactDOM.render(
+  <Router history={history}>
+    <Switch>
+      <Route path="/" exact render={() => <Home />} />
+    </Switch>
+  </Router>,
+  document.getElementById('root'),
+);
 /* eslint-enable react/jsx-filename-extension */
 
 registerServiceWorker();
